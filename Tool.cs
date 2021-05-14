5c8e7472d98a3bb4c02b004ca9809f6a3cbc9898
xiaohai-huang <jeromehuang18@gmail.com>
2021-05-14 13:01:31 +1000

    Initial commit.

---
+++ Tool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment
+{
+    class Tool : iTool
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Quantity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AvailableQuantity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int NoBorrowings { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public iMemberCollection GetBorrowers => throw new NotImplementedException();
+
+        public void addBorrower(iMember aMember)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteBorrower(iMember aMember)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
